de406ce0f74da9b5d8d75b165a48c4891bc3b37c
Christian Fischer <christian@septima.dk>
2021-07-26 11:40:13 +0200

    Opdatering af navn "Parkeringsplads"
    Closes https://github.com/OpenFKG/fkg-postgresql/issues/81

--- src/Upgrade_3_0_1_to_geofa_1_1/upgrade_3_0_3_to_1_1.sql
+++ src/Upgrade_3_0_1_to_geofa_1_1/upgrade_3_0_3_to_1_1.sql
@@ -1,5 +1,17 @@
 BEGIN TRANSACTION;
-INSERT INTO fkg.d_basis_ansvarlig_myndighed VALUES ( 16287180, 'Forsvarsministeriets Ejendomsstyrelse', NULL, 1);
+-- https://github.com/OpenFKG/fkg-postgresql/issues/80
+INSERT INTO fkg.d_basis_ansvarlig_myndighed VALUES ( 16287180, 'Forsvarsministeriets Ejendomsstyrelse', NULL, 1)
+;
+
+-- https://github.com/OpenFKG/fkg-postgresql/issues/81
+UPDATE
+  fkg.d_5800_facilitet 
+SET
+  facil_ty = 'Holdeplads',
+  begrebsdefinition = 'Holdeplads i tilknytning til rekreative områder/faciliteter og som ikke er registreret andre steder. Man kan angive om holdepladsen er egnet til f.eks. hestetrailer i beskrivelsesfeltet.'
+WHERE
+  facil_ty_k = 1291
+;
 
 
 ROLLBACK TRANSACTION;
